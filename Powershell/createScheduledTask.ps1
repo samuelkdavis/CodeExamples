a7f63f52e246963557387fedf3c579a855e5f7c5
samuelkdavis <samuelkdavis@gmail.com>
2024-07-27 00:19:39 +1200

    Add example powershell script

---
+++ Powershell/createScheduledTask.ps1
@@ -0,0 +1,11 @@
+$trigger = New-ScheduledTaskTrigger -AtStartup
+$trigger.Delay = "PT1M"  
+$action = New-ScheduledTaskAction -Execute "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+$principal = New-ScheduledTaskPrincipal -GroupId "BUILTIN\Administrators" -RunLevel Highest
+# 1 minute wait for windows to load critical services/profiles
+$setting = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries
+
+Register-ScheduledTask -TaskName "Start Docker Desktop 1 minute after login" -Action $action -Trigger $trigger -Settings $setting -Principal $principal
+# This will now show up in Task Scheduler in the contents of Task Scheduler Library
+Get-ScheduledTask "Start Docker Desktop 1 minute after login"
+Get-ScheduledTaskInfo "Start Docker Desktop 1 minute after login" 
